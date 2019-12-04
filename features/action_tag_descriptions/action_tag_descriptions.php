6deaec000f870754ad1eb6bd1c91d34107f85ccb
Ed Morales <ed.morales123@partners.org>
2019-12-04 16:31:44 -0500

    fixing duplicated action tag description in action tag description popup

--- features/action_tag_descriptions/action_tag_descriptions.php
+++ features/action_tag_descriptions/action_tag_descriptions.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Partners\swiss_army;
+
 use ExternalModules\ExternalModules as EM;
+
 global $Proj;
 
 $lang = $this->getProjectSetting('language');
@@ -8,39 +11,35 @@ $lang = $this->getProjectSetting('language');
 $Version = EM::getModuleVersionByPrefix('swiss_army');
 
 // Reading hooks config-settings. It is recommended that you leave this in place.
-$config_h_ulr =  __DIR__.'/config.json';
+$config_h_ulr = __DIR__ . '/config.json';
 $contents = file_get_contents($config_h_ulr);
 $contents = utf8_encode($contents);
 $config_h = json_decode($contents, true);
 
 // Generating list of action-tags and their descriptions
-foreach (scandir("../../modules/swiss_army_$Version/features/") as $folder){
-    if (!in_array($folder,array(".",".."))){
+foreach (scandir("../../modules/swiss_army_$Version/features/") as $folder) {
+    if (!in_array($folder, array(".", ".."))) {
 //      Find and read the action-tags' config.json
-        $sub_url= "../../modules/swiss_army_$Version/features/".$folder; // @here
-        foreach(scandir($sub_url) as $file){
+        $sub_url = "../../modules/swiss_army_$Version/features/" . $folder; // @here
+        foreach (scandir($sub_url) as $file) {
             if ($file == 'config.json') {
-                $config_ulr =  "../../modules/swiss_army_$Version/features/".$folder.'/'.$file;
+                $config_ulr = "../../modules/swiss_army_$Version/features/" . $folder . '/' . $file;
                 $contents = file_get_contents($config_ulr);
                 $contents = utf8_encode($contents);
                 $config_x = json_decode($contents, true);
 //              Include actiong tags that have been enabled on the Ext-Mod config
 //              - other conditions can be added too.
-                if($config_x['name'] != 'advanced-tools' and $this->getProjectSetting($config_x['name']) == true and $config_x['type'] == 'action-tag'){
+                if ($config_x['name'] != 'advanced-tools' and $this->getProjectSetting($config_x['name']) == true and $config_x['type'] == 'action-tag') {
                     $desc = $config_x['description'][$lang];
                     $author = $config_x['author-sign'];
                     $ATD = $desc . "<br> <b>Author: </b>" . $author;
-                    $ATName = "@".$config_x['display_name'];
+                    $ATName = "@" . $config_x['display_name'];
                     $add = $config_h['add'][$lang];
                     $AT_default = $config_x['default'];
 
                     $script = <<<SCRIPT
 <script type="text/javascript">
 
-var at_description = '$ATD';
-var at_name = '$ATName';
-var add = '$add';
-var at_default = '$AT_default';
  document.addEventListener("DOMNodeInserted", function(event){
     var element = event.target;
     if (element.tagName == 'DIV') {
@@ -50,16 +49,16 @@ var at_default = '$AT_default';
              "<td class='nowrap' style='text-align:center;background-color:#f5f5f5;color:#912B2B;padding:7px 15px 7px 12px;font-weight:bold;border:1px solid #ccc;border-bottom:0;border-right:0;'>" +
               "<button class='btn btn-xs btn-rcred' style='' " +
                "onclick=$('#field_annotation').val(('" +
-                at_default +
+                '$AT_default' +
                  "&nbsp;'+$('#field_annotation').val())); " +
                 "highlightTableRowOb($(this).parentsUntil('tr').parent(),2500);'>" +
-                 add +
+                 '$add' +
                   "</button></td>" +
                    "<td class='nowrap' style='background-color:#f5f5f5;color:#912B2B;padding:7px;font-weight:bold;border:1px solid #ccc;border-bottom:0;border-left:0;border-right:0;'>" +
-                    at_name +
+                    '$ATName' +
                      "</td>" +
                      "<td style='font-size:12px;background-color:#f5f5f5;padding:7px;border:1px solid #ccc;border-bottom:0;border-left:0;'>" +
-                      at_description +
+                      '$ATD' +
                        "</td>" +
                         "</tr>");
         }
